0c2ea6fb748dd5ff4e9484a7bbe672442e42d7af
Evan Cordell <evan.cordell@localmed.com>
2014-10-16 16:20:34 -0500

    Adds json serialization

---
+++ lib/macaroons/serializers/json.rb
@@ -0,0 +1,28 @@
+require 'json'
+
+module Macaroons
+  class JsonSerializer
+
+    def serialize(macaroon)
+        serialized = {
+          location: macaroon.location,
+          identifier: macaroon.identifier,
+          caveats: macaroon.caveats.map!(&:to_h),
+          signature: macaroon.signature
+        }
+        return serialized.to_json
+    end
+
+    def deserialize(serialized)
+      deserialized = JSON.parse(serialized)
+      macaroon = Macaroons::RawMacaroon.new(key: 'no_key', identifier: deserialized['identifier'], location: deserialized['location'])
+      deserialized['caveats'].each do |c|
+        caveat = Macaroons::Caveat.new(c['cid'], c['vid'], c['cl'])
+        macaroon.caveats << c
+      end
+      macaroon.signature = Utils.unhexlify(deserialized['signature'])
+      macaroon
+    end
+
+  end
+end
